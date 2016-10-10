72c3c681a533cc108308433413d7aad9da848051
James Waples <jamwaffles@gmail.com>
2016-10-10 16:20:47 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,59 @@
+#![crate_type = "staticlib"]
+#![feature(plugin, start, core_intrinsics)]
+#![no_std]
+#![plugin(macro_platformtree)]
+
+extern crate zinc;
+
+use zinc::hal::pin::Gpio;
+
+platformtree!(
+	tiva_c@mcu {
+		clock {
+			source = "PIOSC";
+			xtal   = "X16_0MHz";
+			pll    = true;
+			div    = 5;
+		}
+
+		gpio {
+			f {
+				led1@1 { direction = "out"; }
+				led2@2 { direction = "out"; }
+			}
+		}
+
+		timer {
+			/* The mcu contain both 16/32bit and "wide" 32/64bit timers. */
+			timer@w0 {
+				/* prescale sysclk to 1Mhz since the wait code expects 1us
+				* granularity */
+				prescale = 80;
+				mode = "periodic";
+			}
+		}
+	}
+
+	os {
+		single_task {
+			loop = "run";
+			args {
+				timer = &timer;
+				led1 = &led1;
+				led2 = &led2;
+			}
+		}
+	}
+);
+
+fn run(args: &pt::run_args) {
+	// toggles pin values
+	args.led1.set_high();
+	args.led2.set_low();
+	// wait for 1 second
+	(args.timer as &zinc::hal::timer::Timer).wait(1);
+
+	args.led1.set_low();
+	args.led2.set_high();
+	(args.timer as &zinc::hal::timer::Timer).wait(1);
+}
